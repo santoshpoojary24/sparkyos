454a7749be2c4887bc66fabebe844660a691338f
santoshpoojary24 <25l0050@students.git.edu>
2026-03-29 21:07:24 +0530

    Update user.js

--- api/user.js
+++ api/user.js
@@ -1,9 +1,3 @@
 export default function handler(req, res) {
-    // This tells your frontend: "Yes, this person is a root admin, let them stay on the page!"
-    res.status(200).json({ 
-        user: { 
-            role: 'admin',
-            username: 'Root_Admin'
-        } 
-    });
+  res.status(200).json({ user: { role: 'admin', username: 'Root_Admin' } });
 }
