954500c4256b168ed93d9366614edc72f4f60a2b
santoshpoojary24 <25l0050@students.git.edu>
2026-03-30 10:02:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ let isMaintenanceMode = false;
 let users = [
     { username: 'admin', password: '2468', role: 'admin' },
     { username: 'sparky', password: '1111', role: 'admin' },
-    { username: 'jeevan',  password: '1234',   role: '1234' }
+    { username: 'jeevan',  password: '1234',   role: 'user' }
     { username: 'user',  password: '1234',   role: 'user' }
 ];
 
